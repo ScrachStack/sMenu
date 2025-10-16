704fa3797bb0a16a88149efed12213f40749e8e7
SyncStudios <122732007+ScrachStack@users.noreply.github.com>
2025-10-16 08:39:34 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+-- DO NOT EDIT THIS
+-- JOIN DISCORD FOR SUPPORT!!!
+-- https://disboard.org/server/join/1119734000974565439 or https://discord.com/invite/FaFppQwsgv (just incase discord link expires can use disboard link)
+-- https://disboard.org/server/join/1119734000974565439 or https://discord.com/invite/FaFppQwsgv (just incase discord link expires can use disboard link)
+-- https://disboard.org/server/join/1119734000974565439 or https://discord.com/invite/FaFppQwsgv (just incase discord link expires can use disboard link)
+-- https://disboard.org/server/join/1119734000974565439 or https://discord.com/invite/FaFppQwsgv (just incase discord link expires can use disboard link)
+-- https://disboard.org/server/join/1119734000974565439 or https://discord.com/invite/FaFppQwsgv (just incase discord link expires can use disboard link)
+-- https://disboard.org/server/join/1119734000974565439 or https://discord.com/invite/FaFppQwsgv (just incase discord link expires can use disboard link)
+-- https://disboard.org/server/join/1119734000974565439 or https://discord.com/invite/FaFppQwsgv (just incase discord link expires can use disboard link)
+-- https://disboard.org/server/join/1119734000974565439 or https://discord.com/invite/FaFppQwsgv (just incase discord link expires can use disboard link)
+fx_version 'cerulean'
+games { 'gta5' }
+author 'ScratchStack (https://github.com/ScrachStack/)'
+lua54 'yes'
+client_scripts {
+'client/*'
+}
+server_scripts {
+'server/*'
+}
+shared_script '@ox_lib/init.lua'
+dependency 'ox_lib'
