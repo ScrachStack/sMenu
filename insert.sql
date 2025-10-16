c3f831d09cf7209b2ee7d322be289b7f4c8e89ec
SyncStudios <122732007+ScrachStack@users.noreply.github.com>
2025-10-16 08:51:46 -0500

    Create outfits table with specified columns

---
+++ insert.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `outfits` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(64) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `appearance` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
